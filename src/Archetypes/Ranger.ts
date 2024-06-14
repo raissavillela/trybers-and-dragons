eae9aaf734bb9852eda273d3df49301cb2c1b648
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-06-14 10:37:33 -0700

    Fix lint req 1 feat. req 3, 4 e 5

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,21 @@
+import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
+
+export default class Ranger extends Archetype {
+  private static _instances = 0;
+  private _energyType: EnergyType;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Ranger._instances += 1;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return Ranger._instances;
+  }
+}
